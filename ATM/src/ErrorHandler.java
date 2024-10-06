afd0575cfc6fd8b8f3a0a2f6c058455baf929cd7
Jake Julius D. Maunas <jake22121212@gmail.com>
2024-10-07 06:51:07 +0800

    Delete ATM directory

--- ATM/src/ErrorHandler.java
+++
@@ -1,5 +0,0 @@
-public class ErrorHandler extends Exception{
-    public ErrorHandler(String mess) {
-        super(mess);
-    }
-}
--- ATM/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Atm_Machine ATM = new Atm_Machine();
-    }
-}
